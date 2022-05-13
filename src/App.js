9fc9071309ba36eb62c6346d7446a65167efbcf3
Macbook pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2022-05-13 10:06:37 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import ReactToPrint, { useReactToPrint } from 'react-to-print';
+import { ComponentToPrint } from './ComponentToPrint';
 import './App.css';
+import jsPDF from 'jspdf';
 
 function App() {
+  const componentRef = useRef();
+  // React.forwardRef((props, ref) => <NavLink innerRef={ref} {...props} />);
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+ const generatePDF = ()=> {
+    let doc = new jsPDF ( "p","pt","a4");
+    doc.html(document.querySelector("#content"), {
+      callback: function(pdf){
+        pdf.save ( "mypdf.pdf");
+      }
+    })
+ }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <ReactToPrint
+        trigger={() => <button>Print this out!</button>}
+        content={() => componentRef.current} */}
+      {/* /> */}
+      
+      <div id="content">
+      <ComponentToPrint  ref={componentRef} />
+      </div>
+      <button onClick={()=>generatePDF()}>Download</button>
+      <button onClick={handlePrint}>
+        Print
+      </button>
     </div>
+   
   );
 }
 
